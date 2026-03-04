3f816aa357569f9f5f9f3fb4c9cf4fc45c96331d
Mario Romero <marioromero.ttp@gmail.com>
2026-03-03 23:26:07 -0300

    Environments OK

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  serviceUrl: 'https://ggxdsvifoqffxvqnfpsf.supabase.co',
+  serviceKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdneGRzdmlmb3FmZnh2cW5mcHNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODc1NzYsImV4cCI6MjA4ODA2MzU3Nn0.PaamZWYHlBI5ewdpQDrdfKI2iw-k7CBN40drzEfRP8Y'
+};
