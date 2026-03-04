63be739f71350f8b97a7260d1ea5323528ff17d9
Mario Romero <marioromero.ttp@gmail.com>
2026-03-04 10:58:12 -0300

    Componentes html para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    // Usamos Lazy Loading. Esto hace que Angular solo descargue el código del Login cuando el usuario visite esta URL.
+    loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', // Si escriben una URL que no existe, los mandamos al login
+    redirectTo: 'login'
+  }
+];
